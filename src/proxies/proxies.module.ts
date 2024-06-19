53408cd3b48efe401ab6f04108070ee77b4a2faf
songil88620 <songil88620@gmail.com>
2024-06-20 03:44:12 +0800

    update

--- src/proxies/proxies.module.ts
+++ src/proxies/proxies.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ProxiesService } from './proxies.service';
 import { ProxiesController } from './proxies.controller';
+import { ProxyEntity } from './entities/proxy.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [ 
+    TypeOrmModule.forFeature(
+      [ProxyEntity]
+    )
+  ],
   controllers: [ProxiesController],
   providers: [ProxiesService],
   exports: [ProxiesService]
