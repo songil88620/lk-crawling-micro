03ef303eadf4568a51de4e768cbfba10eeb06845
songil88620 <songil88620@gmail.com>
2024-08-19 21:43:31 +0800

    debug

--- src/bot/bot.service.ts
+++ src/bot/bot.service.ts
@@ -1627,6 +1627,7 @@ export class BotService {
             if (u_name) {
                 var n = await (await u_name.getProperty('textContent')).jsonValue()
                 const home = this.beautySpace(n);
+                console.log(">>>HOMe", home)
                 if (home == 'Home') {
                     this.lang = 0;
                 } else if (home == 'Inicio') {
