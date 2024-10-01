62ebd9a987929eaf6824a597b67377828d1f1f02
songil88620 <songil88620@gmail.com>
2024-10-01 18:26:03 +0800

    debug

--- src/bot/bot.service.ts
+++ src/bot/bot.service.ts
@@ -2033,17 +2033,17 @@ export class BotService {
                     try {
 
                         // to debug class
-                        // const elementHandle3 = await my_page.$('.msg-overlay-list-bubble__content--scrollable');
-                        // if (elementHandle3) {
-                        //     console.log(">>>>333 here")
-                        //     const componentHTML = await my_page.evaluate(element => element.outerHTML, elementHandle3);
-                        //     const componentClass = await my_page.evaluate(element => element.className, elementHandle3);
-                        //     console.log('Component HTML:', componentHTML);
-                        //     console.log('Component Class:', componentClass);
+                        const elementHandle3 = await my_page.$('.scrollable msg-overlay-list-bubble__content');
+                        if (elementHandle3) {
+                            console.log(">>>>333 here")
+                            const componentHTML = await my_page.evaluate(element => element.outerHTML, elementHandle3);
+                            const componentClass = await my_page.evaluate(element => element.className, elementHandle3);
+                            console.log('Component HTML:', componentHTML);
+                            console.log('Component Class:', componentClass);
 
-                        // } else {
-                        //     console.log(">>>333 not exist")
-                        // }
+                        } else {
+                            console.log(">>>333 not exist")
+                        }
 
                         const new_comp = await my_page.$('.msg-overlay-list-bubble__default-conversation-container .entry-point:nth-child(' + sid + ')');
                         if (new_comp) {
