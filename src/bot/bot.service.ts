432b8d29ec46cac6203c9a2eb6e9c74569f4adca
songil88620 <songil88620@gmail.com>
2024-10-01 18:14:09 +0800

    debug

--- src/bot/bot.service.ts
+++ src/bot/bot.service.ts
@@ -1963,7 +1963,7 @@ export class BotService {
                 try {
                     const close_btn_msgbox = '.msg-overlay-bubble-header__controls button:last-child';
                     await my_page.waitForSelector(close_btn_msgbox);
-                    await my_page.click(close_btn_msgbox);
+                   // await my_page.click(close_btn_msgbox);
                     await this.delay(1500)
                 } catch (e) {
                     console.log(">>>component error 1664:", e)
